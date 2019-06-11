2a4d327a7453d2eb1a2d51a25fcc98c49d624608
MariaZanahoria <m.iselacarrillo.p@gmail.com>
2019-06-11 01:30:56 -0500

    Diseño a botones e input type

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,39 @@
 window.cipher = {
 // Funcion de cambiar el texto a ASCII
-encodeAscii = (string) => {
-  let caracteres = [];
-    valor = textToEncode.value
-  const upperCaseString = valor.toUpperCase();
-  for (let c = 0; c < upperCaseString.length; c++){
-    caracteres[c] = upperCaseString.charAt(c).charCodeAt(0);
-} 
+    encodeAscii = (string) => {
+      let caracteres = [];
+        valor = textToEncode.value
+      const upperCaseString = valor.toUpperCase();
+      for (let c = 0; c < upperCaseString.length; c++){
+        caracteres[c] = upperCaseString.charAt(c).charCodeAt(0);
+   } 
 return caracteres
 },
 // Funncion de cambiar el arreglo ASCII apicando desplazamiento
-cipherText = (array, offset) => {
-  let cipher = [];
-  for (let c = 0; c <array.length; c++){
-  let cifrado = (array[c] - 65 + offset)%26+65;
-    cipher[c]=parseInt(cifrado);
-}
+    cipherText = (array, offset) => {
+      let cipher = [];
+      for (let c = 0; c <array.length; c++){
+      let cifrado = (array[c] - 65 + offset)%26+65;
+        cipher[c]=parseInt(cifrado);
+    }
 return cipher
 },
 // Funcion de convertir un arreglo ASCII a un string
-convertAsciiToString = (array) => {
-  let encodedString = "";
-  for(let c = 0; c < array.length; c++){
-  let char = String.fromCharCode(array[c]);
-    encodedString += char;
-}
+    convertAsciiToString = (array) => {
+      let encodedString = "";
+      for(let c = 0; c < array.length; c++){
+      let char = String.fromCharCode(array[c]);
+        encodedString += char;
+    }
 return encodedString
 },
 // Funcion de decodificar aplicando desplazamiento
-cipherTextDec = (array, offset) => {
-  let cipherDec = [];
-  for (let i = 0; i <array.length; i++){
-  let cifradoDec = (array[i] - offset - 65)%26+65;
-    cipherDec[i]=parseInt(cifradoDec);
-}
+    cipherTextDec = (array, offset) => {
+      let cipherDec = [];
+      for (let i = 0; i <array.length; i++){
+      let cifradoDec = (array[i] - offset - 65)%26+65;
+        cipherDec[i]=parseInt(cifradoDec);
+    }
 return cipherDec
 }
 }
